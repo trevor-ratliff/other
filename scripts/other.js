879dc63ca4438c2b1da6a19487e8cba7cd393dda
Trevor Ratliff <trevor.w.ratliff@gmail.com>
2013-12-05 17:48:08 -0500

    a bunch of work on my 'other' pages

---
+++ scripts/other.js
@@ -0,0 +1,29 @@
+//====
+/// @file other.js
+/// @brief holds site wide javascript
+/// @author Trevor Ratliff
+/// @date 2013-12-04
+//
+/// @verbatim
+/// History:  Date  |  Programmer  |  Contact  |  Description  |
+///     2013-12-04  |  Trevor Ratliff  |  trevor.w.ratliff@gmail.com  |  
+///         file creation  |
+/// @endverbatim
+//====
+
+//----
+// setup breadcrumbs
+//----
+var objCrumbs = new BreadCrumb({});
+objCrumbs.Add(window.location.toString(), document.title);
+
+
+window.addEventListener('load', function (vobjEvent) {
+	//----
+	// do stuff here for page load
+	//----
+	//~ alert('hello');
+	var lstrCrumbs = objCrumbs.Generate('style');
+	
+	document.querySelector('#breadcrumbs').innerHTML = lstrCrumbs;
+});
