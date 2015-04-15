5150ba26ea32de1dd4d8c38c84b3f0e55683a89b
Trevor Ratliff <trevor.w.ratliff@gmail.com>
2015-04-15 17:31:30 -0400

    moved some stuff and added others

---
+++ programming/polymer/scripts/learningPolymer.js
@@ -0,0 +1,31 @@
+//====
+/// @file 
+/// @brief 
+/// @author 
+/// @date 
+//  
+//  Definitions:
+//  
+/// @verbatim
+/// History:  Date  |  Programmer  |  Contact  |  Description  |
+/// @endverbatim
+//====
+
+
+//====
+/// @fn MyFunction(my_param)
+/// @brief 
+/// @author 
+/// @date 
+/// @param my_param -- 
+/// @return
+//  
+//  Definitions:
+//  
+/// @verbatim
+/// History:  Date  |  Programmer  |  Contact  |  Description  |
+/// @endverbatim
+//====
+function My_Function(my_param){
+    
+}
