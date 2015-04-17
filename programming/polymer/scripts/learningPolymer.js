ddc53f1dc6cc845b94c15cee5e445deda1cab3a6
Trevor Ratliff <trevor.w.ratliff@gmail.com>
2015-04-17 16:48:22 -0400

    got web component working via polymer

--- programming/polymer/scripts/learningPolymer.js
+++ programming/polymer/scripts/learningPolymer.js
@@ -1,31 +1,76 @@
 //====
-/// @file 
-/// @brief 
-/// @author 
-/// @date 
+/// @file learningPolymer.js
+/// @brief javascript to run learning page
+/// @author Trevor Ratliff
+/// @date 2015-04-16
 //  
 //  Definitions:
+//  	addOne(vstrId) -- adds a row to the row-repeat element specified
+//  	subOne(vstrId) -- subtracts a row from the element
 //  
 /// @verbatim
 /// History:  Date  |  Programmer  |  Contact  |  Description  |
+///		2015-04-16  |  Trevor Ratliff  | trevor.w.ratliff@gmail.com  |
+///			file creation  |
+///		2015-04-17  |  Trevor Ratliff  | trevor.w.ratliff@gmail.com  |
+///			adding addOne and subOne functions  |
 /// @endverbatim
 //====
 
 
 //====
-/// @fn MyFunction(my_param)
-/// @brief 
-/// @author 
-/// @date 
-/// @param my_param -- 
-/// @return
+/// @fn addOne (vstrId)
+/// @author Trevor Ratliff
+/// @date 2015-04-17
+/// @param vstrId -- id of row repeater to modify
+/// @return int -- the new count
 //  
 //  Definitions:
+//      lobjRowRepeater -- reference to row repeater element
 //  
 /// @verbatim
 /// History:  Date  |  Programmer  |  Contact  |  Description  |
+///     2015-04-17  |  Trevor Ratliff  |  trevor.w.ratliff@gmail.com  |
+///         function creation  |
 /// @endverbatim
 //====
-function My_Function(my_param){
-    
+function addOne (vstrId) {	// returns int
+	var lobjRowRepeater = document.getElementById(vstrId);
+
+	//----
+	// set count
+	//----
+	lobjRowRepeater.count++;
+
+	return lobjRowRepeater.count;
 }
+
+
+//====
+/// @fn subOne (vstrId)
+/// @author Trevor Ratliff
+/// @date 2015-04-17
+/// @param vstrId -- id of row repeater element
+/// @return int --the new count
+//  
+//  Definitions:
+//      lobjRowRepeater -- reference to repeater element
+//  
+/// @verbatim
+/// History:  Date  |  Programmer  |  Contact  |  Description  |
+///     2015-04-17  |  Trevor Ratliff  |  trevor.w.ratliff@gmail.com  |
+///         function creation  |
+/// @endverbatim
+//====
+function subOne (vstrId) {	//returns int
+	var lobjRowRepeater = document.getElementById(vstrId);
+
+	//----
+	// set count
+	//----
+	lobjRowRepeater.count = lobjRowRepeater.count < 1 ? 0 : 
+		lobjRowRepeater.count - 1;
+
+	return lobjRowRepeater.count;
+}
+
