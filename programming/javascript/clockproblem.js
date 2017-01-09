aaddddb337dec3afd41a84b0f56cec9761ebfd1c
Trevor Ratliff <trevor.w.ratliff@gmail.com>
2017-01-09 14:44:40 -0500

    fixed the problem
    done on an analog clock with an hour hand that moves each minute

--- programming/javascript/clockproblem.js
+++ programming/javascript/clockproblem.js
@@ -6,10 +6,12 @@ the solution for Richmond VA - Chicago IL is: 12:30, 6:30
 */
 var hourAngles = [];
 var minAngles = [];
-var timeHourDiff = -1;
-var timeMinDiff = 0;
 
-for (let ii = 0; ii < 360; ii += (360/12)) {
+// everything is in minutes so we'll express the difference in minutes as well
+//var timeHourDiff = -1;
+var timeMinDiff = -60;	
+
+for (let ii = 0; ii < 360; ii += (360/(12 * 60))) {
   hourAngles.push(ii);
 }
 
@@ -17,7 +19,8 @@ for (let ii = 0; ii < 360; ii += (360/60)) {
   minAngles.push(ii);
 }
 
-for (let ii = 12; ii < 24; ii++) {
+// loop through hours then minutes
+/*for (let ii = 12; ii < 24; ii++) {
   for (let nn = 0; nn < 60; nn++) {
     var hourOffset = (nn + timeMinDiff) > 60 ? 1 : 0;
     var hourUnderset = (nn - timeMinDiff) < 0 ? -1 : 0;
@@ -33,6 +36,28 @@ for (let ii = 12; ii < 24; ii++) {
     if (mainSite == sitePlus || (360 - mainSite) == sitePlus)
       console.log(`${currHour}:${(currMin < 10 ? '0' + currMin : currMin)} - mainSite: ${mainSite}(${(360-mainSite)}) - siteMinus: ${siteMinus}, sitePlus: ${sitePlus}`);
   }
+}*/
+
+// loop through as minutes
+for (let min = 0; min < 720; min++) {
+  let strHr = Math.floor(min/60).toString();
+  if (strHr === "0") strHr = "12";
+  
+  let currMin = min % 60;
+  let strMin = currMin < 10 ? '0' + currMin.toString() : currMin.toString();
+
+  let minPlusAdjust = min + timeMinDiff;
+  if (minPlusAdjust < 0) minPlusAdjust += 720;
+
+  let minMinusAdjust = min - timeMinDiff;
+  if (minMinusAdjust > 720) minMinusAdjust -= 720;
+
+  var mainSite = findAngle(min, min %60);
+  var sitePlus = findAngle((minPlusAdjust) % 720, min % 60);
+  var siteMinus = findAngle((minMinusAdjust) % 720, min % 60);
+
+  if ((mainSite === sitePlus || (360 - mainSite) === sitePlus))// || (mainSite === siteMinus || (360 - mainSite) === siteMinus)) 
+    console.log(`${strHr}:${strMin} - mainSite: ${mainSite} - sitePlus: ${sitePlus} - siteMinus = ${siteMinus}`);
 }
 
 function findAngle(hour, min) {
