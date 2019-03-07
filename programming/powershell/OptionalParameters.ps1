68caffba11f3bae23024bf4624ee8eaabb41d114
Trevor Ratliff <20759839@gs.acs-inc.com>
2019-03-07 18:39:50 -0500

    updating optional parameter test script

--- programming/powershell/OptionalParameters.ps1
+++ programming/powershell/OptionalParameters.ps1
@@ -4,13 +4,14 @@ Param (
   [string]$test1,
   [Parameter(Mandatory=$true, Position=2, HelpMessage="Second manditory parameter")]
   [string]$test2,
-  [string]$optional1=$null,
+  [string]$optional1=((Read-Host -Prompt "Optional Parameter 1 [optional1]") -replace "^$", "optional1"),
   [string]$optional2=$null,
   [switch]$doTwice,
   [string]$version="20171221.1"
 )
 Write-Verbose "getting value from user"
-$user1 = Read-Host -Prompt "Please enter the first user value"
+#$user1 = Read-Host -Prompt "Please enter the first user value"
+$user1 = (Read-Host -Prompt "user Parameter 1 [user1]") -replace "^$", "user1"
 
 Write-Verbose "setting test variable to see if optional variable 2 is null"
 
@@ -33,10 +34,10 @@ if ($optional2 -ne '') {
 }
 
 Write-Verbose "writting values`n`n"
-Write-Host $test1 $test2 $_optional1 $_optional2 $version
+Write-Host $test1 $test2 $user1 $_optional1 $_optional2 $version
 
 if ($doTwice) {
-  Write-Host $test1 $test2 $_optional1 $_optional2 $version `n
+  Write-Host $test1 $test2 $user1 $_optional1 $_optional2 $version `n
 }
 
 Write-Verbose "splitting up version data`n`n"
