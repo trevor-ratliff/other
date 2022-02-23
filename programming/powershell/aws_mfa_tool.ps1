e1419578539ac0ab594cd69b24258751736c9c3c
Trevor Ratliff <tratliff@qsrautomations.com>
2022-02-23 09:55:39 -0600

    tweaking the way parameters work so I could add a -defaults flag

--- programming/powershell/aws_mfa_tool.ps1
+++ programming/powershell/aws_mfa_tool.ps1
@@ -1,39 +1,79 @@
 [CmdletBinding()]
 Param (
   [Parameter(Mandatory=$false, HelpMessage="The AWS Account ID for the group")]
-  [string]$accountId=((Read-Host -Prompt "Please enter the AWS Account ID []") -replace '^$', ''),
+  [string]$accountId,
 
   [Parameter(Mandatory=$false, HelpMessage="The profile to use for generating the session token")]
-  [string]$useProfile=((Read-Host -Prompt 'AWS Profile Name [default]') -replace '^$', 'default'),
+  [string]$useProfile,
 
   [Parameter(Mandatory=$false, HelpMessage="The IAM user for this connection")]
-  #[string]$iamUser=((Read-Host -Prompt 'IAM User [tratliff]') -replace '^$', 'tratliff'),
-  [string]$iamUser=((Read-Host -Prompt "IAM User [$env:UserName]") -replace '^$', $env:UserName),
+  [string]$iamUser,
 
   [Parameter(Mandatory=$false, HelpMessage="The path to your AWS CLI credential file")]
-  [string]$credentialFilePath=((Read-Host -Prompt "credential path [$home\.aws\credentials]") -replace '^$', "$home\.aws\credentials"),
+  [string]$credentialFilePath,
 
   [Parameter(Mandatory=$false, HelpMessage="The time the token should be valid in hours (between 12 and 36) [12]")]
-  [double]$tokenLifetimeHours=(Read-Host -Prompt "The time the token should be valid in hours (between 12 and 36) [12]"),
+  [double]$tokenLifetimeHours,
 
   [Parameter(Mandatory=$false, HelpMessage="The profile that will be updated or created from the results of this execution")]
-  [string]$updateProfile=((Read-Host -Prompt 'Update profile [qsrdev]') -replace '^$', 'qsrdev'),
+  [string]$updateProfile,
 
   [Parameter(Mandatory=$false, HelpMessage="The multi-factor authentication token")]
-  [string]$tokenCode=(Read-Host -Prompt 'MFA Token Code'),
+  [string]$tokenCode,
 
   [string]$region="us-east-1",
   [switch]$showResults,
   [switch]$noProfileUpdate,
   [switch]$version,
+  [switch]$defaults,
+  [switch]$d,
   [switch]$whatIf
 )
-[string]$scriptVersion="20220111.2"
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+[string]$scriptVersion="20220222.0"
 
 Write-Host ("`n`nWelcome to the QSR AWS Multifactor Session Tool (version $scriptVersion).`n`n"+
             "    This tool will generate a temporary session token for AWS services when `n"+
             "multi-factor authentication is enabled.  It will put the temporary token `n"+
             "in environment variables as well as your AWS CLI credentials file. `n`n")
+#----
+# test for default parameter: if present set smart defaults and only ask for pin
+#----
+if ($default -or $d) {
+    $accountId = '964160303629'
+    $useProfile = 'userdev'
+    $iamUser = $env:UserName
+    $credentialFilePath = "$home\.aws\credentials"
+    $tokenLifetimeHours = 12
+    $updateProfile = 'default'
+} else {
+    if ($accountId -eq $null -or $accountId -eq '') {
+        $accountId=((Read-Host -Prompt "Please enter the AWS Account ID`n  QSR Dev = `n  QSR QA = `n  QSR Prod = `nDefaults to QSR Dev []") -replace '^$', '')
+    }
+
+    if ($useProfile -eq $null -or $useProfile -eq '') {
+        $useProfile=((Read-Host -Prompt 'AWS User Profile Name [userdev]') -replace '^$', 'userdev')
+    }
+
+    if ($iamUser -eq $null -or $iamUser -eq '') {
+        $iamUser=((Read-Host -Prompt "IAM User [$env:UserName]") -replace '^$', $env:UserName)
+    }
+
+    if ($credentialFilePath -eq $null -or $credentialFilePath -eq '') {
+        $credentialFilePath=((Read-Host -Prompt "credential path [$home\.aws\credentials]") -replace '^$', "$home\.aws\credentials")
+    }
+
+    if ($tokenLifetimeHours -eq $null -or $tokenLifetimeHours -eq '') {
+        $tokenLifetimeHours=(Read-Host -Prompt "The time the token should be valid in hours (between 12 and 36) [12]")
+    }
+
+    if ($updateProfile -eq $null -or $updateProfile -eq '') {
+        $updateProfile=((Read-Host -Prompt 'Update profile [default]') -replace '^$', 'default')
+    }
+}
+if ($tokenCode -eq $null -or $tokenCode -eq '' -and $version -ne $true) {
+    $tokenCode=(Read-Host -Prompt 'MFA Token Code')
+}
 
 Write-Verbose ("`nVerifying parameters`n")
 
@@ -102,6 +142,7 @@ if ($noProfileUpdate -ne $true) {
     ## aws_secret_access_key= SECRET_ACCESS_KEY
     ## aws_session_token= SESSION_TOKEN
     ## region=us-east-1
+    ## # this token will expire at EXPIRATION
     [string]$newCreds = (("{0}]¦" +
         "aws_access_key_id={1}¦" +
         "aws_secret_access_key={2}¦" +
@@ -128,9 +169,9 @@ if ($noProfileUpdate -ne $true) {
     Write-Verbose ("`nUpdating profile [$updateProfile]`n")
     [bool]$foundProfile = $false
     for ($ii=0; $ii -lt $credGroups.Length; $ii++){
-        Write-Debug ("  Comparing <[{0}]>" -f $credGroups[$ii])
-        Write-Debug ("    contains $updateProfile = [{0}]" -f $credGroups[$ii].Contains($updateProfile))
-        if ($credGroups[$ii].Contains($updateProfile)) {
+        Write-Debug ("  Comparing <<{0}>>" -f $credGroups[$ii])
+        Write-Debug ("    contains <<$updateProfile]>> = <<{0}>>" -f ($credGroups[$ii] -match ('^' + $updateProfile +'\]')))
+        if ($credGroups[$ii] -match ('^' + $updateProfile +'\]')) {
             Write-Debug ("    replacing old value`n")
             $credGroups[$ii] = $newCreds
             $foundProfile = $true
@@ -142,15 +183,16 @@ if ($noProfileUpdate -ne $true) {
         $credGroups[$credGroups.Length-1] += "¦[$newCreds"
     }
 
-    Write-Debug ("`nprofile count [{0}]`nnew file content`n{1}`n" -f $credGroups.Length, $credGroups.ToString())
+    Write-Debug ("`nprofile count [{0}]`nnew file content`n{1}`n" -f $credGroups.Length, ($credGroups -join "`n"))
 
     Write-Verbose ("`nWriting updated credentials file`n")
-    [string]$newCredentialFile = ($credGroups -Join("[")) -Replace "¦","`n"
+    # [string]$newCredentialFile = ($credGroups -Join("[")) -Replace "¦","`n"
+    [string]$newCredentialFile = ($credGroups -Join("[")) -Replace "¦",[ENVIRONMENT]::NewLine
     Write-Debug ("`nFinal file update (length [{0}])`n{1}`n" -f $newCredentialFile.Length, $newCredentialFile)
     if ($whatIf -ne $true) {
         Set-Content -Path $credentialFilePath -Value $newCredentialFile
     }
 }
 
-Write-Host ("The AWS session token will expire at {0}" -f $credentials.Credentials.Expiration)
+Write-Warning ("`nThe AWS session token will expire at {0}`n" -f $credentials.Credentials.Expiration)
 exit 0
