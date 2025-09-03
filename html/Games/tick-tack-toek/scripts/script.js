bec671fcd17ef0e581a698a8812c30cc0938ece6
Trevor Ratliff <trevor.w.ratliff@gmail.com>
2025-09-03 12:36:16 -0500

    updating some files

--- html/Games/tick-tack-toek/scripts/script.js
+++ html/Games/tick-tack-toek/scripts/script.js
@@ -1,9 +1,17 @@
-let _app = { cons: console || {} };
+let _app = { 
+    cons: console || {},
+    turn: 'x',
+    hasWon: false
+};
 
 (function (self){
+    // add greeting
     if (!!_app.cons && !!_app.cons.log) {
         _app.cons.log("Hello from Tick-Tack-Toek");
     } else {
         self.alert("hello from tick-tack-toek");
     }
+
+    // add click handler to '.grid-item'
+    
 })(this);
